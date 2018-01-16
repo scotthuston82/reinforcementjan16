ba6ad189a02df2019cdfbc46ed5ae345adf059c8
Scott <scotthuston1982@gmail.com>
2018-01-16 16:29:15 -0500

    finished

---
+++ exercise.rb
@@ -0,0 +1,15 @@
+numbers = {}
+
+50.times do |i|
+  if (i + 1) % 2 == 0 && (i + 1) % 7 == 0
+    numbers[(i + 1)] = ((i + 1) * 2)
+  elsif (i + 1) % 2 == 0
+    numbers[(i + 1)] = (i + 2)
+  elsif (i + 1) % 7 == 0
+    numbers[(i + 1)] = i
+  else
+    numbers[(i + 1)] = (i + 1)
+  end
+end
+
+p numbers
